7564c8cc60f09b58416581ba76c9a038f7e2323e
Favour F Atilade <favyatil@gmail.com>
2025-04-08 01:16:57 +0200

    FIX: bcrypt dependency replaced with bcryptjs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+webpack: (config: { externals: string[]; }) => {
+    config.externals.push("bcrypt");
+    return config;
+  },
 };
+
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+  //webpack: (config: { externals: string[]; }) => {
+    //config.externals.push("bcrypt");
+    //return config;
+  //},
+//};
 
 export default nextConfig;
