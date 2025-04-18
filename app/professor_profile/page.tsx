7b87158ea54a35d32c01420b739af6f7005bc0d1
unknown <stefany.hf9@gmail.com>
2025-04-18 18:39:47 +0200

    Created Routes, basic page files, and placeholder actions

---
+++ app/professor_profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfessorProfilePage() {
+    return (
+      <div>
+        <h1>Professor Profile</h1>
+        <p>This is the Professor Profile page.</p>
+      </div>
+    )
+  }
+  
